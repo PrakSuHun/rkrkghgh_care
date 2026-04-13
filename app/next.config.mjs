bf37af24ae7439b322e40b40310048010280d5ad
PrakSuHun <hyhyxodh@gmail.com>
2026-04-13 15:57:25 +0900

    Remove invalid turbopackFileSystemCacheForDev experimental flag

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -2,9 +2,6 @@ const isVercel = !!process.env.VERCEL;
 
 const config = {
   ...(isVercel ? {} : { distDir: "/tmp/care-next" }),
-  experimental: {
-    turbopackFileSystemCacheForDev: false,
-  },
 };
 
 export default config;
